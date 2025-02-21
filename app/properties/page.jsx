018e59bc485e136039a1f14fda10161afe81e5ce
ashwin_shah <ashwin_shah@dell.com>
2025-02-21 14:48:23 +0530

    Pulling properties form mongodb for listing of properties
    Removing the static reference to properties.json and connecting to mongo db for pulling of  property information.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,12 +1,26 @@
-import properties from '@/properties.json';
+
 import PropertyCard from '@/components/PropertyCard';
 
-const PropertiesPage = () => {
-    console.log("properties server testing")
+async function fetchProperties() {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMIAN}/properties`);
+        if (!res.ok) {
+            throw new Error("Faield to Fetch Data")
+        }
+        return res.json();
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+const PropertiesPage = async () => {
+    const properties = await fetchProperties();
+    console.log("properties server testing", properties)
     return (
-        <section class="px-4 py-6">
-            <div class="container-xl lg:container m-auto px-4 py-6">
-                {properties.length === 0 ? <p>No properties found.</p> : (<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+                {properties.length === 0 ? <p>No properties found.</p> : (<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {properties.map((property) => (
                         <PropertyCard key={property._id} property={property} />
                     ))}
