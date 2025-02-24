15c724353a4fb5e80ee9a39155bdf27097fde871
ashwin_shah <ashwin_shah@dell.com>
2025-02-24 22:15:23 +0530

    adding Authorization

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,8 @@
+import { AuthOptions } from '@/utils/authOptions';
+
+import NextAuth from 'next-auth/next';
+
+console.log("Inside Route");
+const handler = NextAuth(AuthOptions);
+console.log("Inside Route");
+export { handler as GET, handler as POST }
