10e6dffa657f06e8eddcb7f9fe7081f69b8b472e
ashwin_shah <ashwin_shah@dell.com>
2025-02-27 16:24:35 +0530

    Post request and data building for the property add route

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -12,4 +12,47 @@ export const GET = async (request) => {
         console.log(error)
         return new Response("Something went wrong", { status: 500 })
     }
-} 
+}
+
+export const POST = async (request) => {
+    try {
+        const formData = await request.formData()
+        const amenities = formData.getAll('amenities')
+        const images = formData
+            .getAll('images')
+            .filter((image) => image.name !== '')
+
+        const propertyData = {
+            name: formData.get("name"),
+            type: formData.get("type"),
+            description: formData.get("description"),
+            location: {
+                street: formData.get("location.street"),
+                city: formData.get("location.city"),
+                state: formData.get("location.state"),
+                zipcode: formData.get("location.zipcode")
+            },
+            beds: formData.get("beds"),
+            baths: formData.get("baths"),
+            square_feet: formData.get("square_feet"),
+            amenities,
+            rates: {
+                weekly: formData.get("rates.weekly"),
+                monthly: formData.get("rates.monthly"),
+                nightly: formData.get("rates.nightly")
+            },
+            seller_info: {
+                name: formData.get("seller_info.name"),
+                email: formData.get("seller_info.email"),
+                phone: formData.get("seller_info.phone")
+            },
+            images
+        }
+
+        console.log(propertyData)
+        return new Response(JSON.stringify({ message: 'testing post message for properties page  success' }), { status: 200 })
+    } catch (error) {
+
+        return new Response(JSON.stringify({ message: 'Some error happend in POST call in properties route page' }), { status: 500 })
+    }
+}
