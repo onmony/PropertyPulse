ee0917a42f0af051c85f96db8564fdbb6f197163
ashwin_shah <ashwin_shah@dell.com>
2025-02-27 21:24:15 +0530

    Create Add Properties and cloudinary integration

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -8,6 +8,7 @@ import { FaArrowLeft } from 'react-icons/fa'
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import PropertyDetails from '@/components/PropertyDetails';
 import Spinner from '@/components/Spinner';
+import PropertyImages from '@/components/PropertyImages'
 
 
 const PropertyPage = () => {
@@ -152,6 +153,7 @@ const PropertyPage = () => {
                             </div>
                         </div>
                     </section>
+                    <PropertyImages images={property.images} />
                 </>
             )}
         </>
