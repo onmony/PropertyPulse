34f54cfc2df97b61d65bf80c8c1652bed4c95140
ashwin_shah <ashwin_shah@dell.com>
2025-02-28 11:59:27 +0530

    Profile page building and individual user property listin

---
+++ app/api/properties/user/[userid]/route.jsx
@@ -0,0 +1,20 @@
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+
+// /api/properties/user/:userId
+export const GET = async (request, { params }) => {
+    try {
+        await connectDB();
+        const userId = params.userid;
+        if (!userId) {
+            return new Response("UserId is required field", { status: 400 })
+        }
+        const properties = await Property.find({ owner: userId })
+
+        return new Response(JSON.stringify(properties), { status: 200 })
+    }
+    catch (error) {
+        console.log(error)
+        return new Response("Something went wrong", { status: 500 })
+    }
+}
